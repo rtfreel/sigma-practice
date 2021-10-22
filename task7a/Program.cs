c26a7429842aa203c27d49203b4667e2f2a7a787
Roman Tkachenko <rt4freel@gmail.com>
2021-10-22 17:39:57 +0300

    task 7a

---
+++ task7a/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task7a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cont = true;
+            Translator t = new Translator();
+            string input;
+            List<string> unknown;
+            while (cont)
+            {
+                Console.WriteLine("Please, enter sentence to translate:");
+                input = Console.ReadLine();//"Hello! My name   is Roman, and yours?";
+                unknown = t.Check(input);
+                if (unknown.Count > 0) Console.WriteLine("\nProvide translation to replace following words:");
+                foreach (string word in unknown)
+                {
+                    Console.Write(word + " -> ");
+                    t.Add(word, Console.ReadLine());
+                }
+                Console.WriteLine("\nResult:\n" + t.Translate(input));
+
+                Console.Write("\nDo you want to continue with current dictionary? (y/n) ");
+                cont = Console.ReadLine().ToLower().Equals("y");
+                Console.WriteLine("\nCurrent dictionary:\n" + DictToStr(t.GetDict()));
+            }
+        }
+        static string DictToStr(Dictionary<string, string> dict)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(KeyValuePair<string, string> kv in dict)
+            {
+                sb.Append($"\"{kv.Key}\" -> \"{kv.Value}\"\n");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ task7a/Translator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace task7a
+{
+    class Translator
+    {
+        private static readonly char[] delimiters = { ' ', ',', '!', '?', '\'', '"', '.', ':', '\t', '\n'};
+        private Dictionary<string, string> dict;
+
+        public Translator()
+        {
+            dict = new Dictionary<string, string>();
+        }
+
+        public Translator(Dictionary<string, string> dict)
+        {
+            this.dict = dict;
+        }
+
+        public void Add(string key, string value)
+        {
+            dict.Add(key.Trim().ToLower(), value.Trim().ToLower());
+        }
+
+        public Dictionary<string, string> GetDict() { return dict; }
+
+        //returs unknown words
+        public List<string> Check(string input)
+        {
+            List<string> result = new List<string>();
+            string[] words = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            bool known;
+            for (int i = 0; i < words.Length; i++) {
+                words[i] = words[i].ToLower();
+                known = false;
+                foreach (KeyValuePair<string, string> kv in dict)
+                {
+                    if (words[i].Equals(kv.Key))
+                    {
+                        known = true;
+                        break;
+                    }
+                }
+                if (!known) result.Add(words[i]);
+            }
+            return result;
+        }
+
+        public string Translate(string input)
+        {
+            string result = input;
+            foreach (KeyValuePair<string, string> kv in dict)
+            {
+                result = Regex.Replace(result, kv.Key, match =>
+                {
+                    bool isUpper = char.IsUpper(match.Value[0]);
+                    char[] word = kv.Value.ToCharArray();
+                    word[0] = isUpper
+                        ? char.ToUpper(word[0])
+                        : char.ToLower(word[0]);
+                    return new string(word);
+                }, RegexOptions.IgnoreCase);
+            }
+            return result;
+        }
+    }
+}
